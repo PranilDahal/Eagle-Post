e2bdb3f64d6bda70d08bddc996effeaf1b04acea
PranilDahal <dahalpranil@gmail.com>
2018-08-14 19:28:57 -0700

    Frontend Update - Created sub components for SSU list.

--- frontend/src/components/SsuList.vue
+++ frontend/src/components/SsuList.vue
@@ -4,50 +4,10 @@
 		<div class="container col-lg-8">
 
 			<!-- ******** this is where individual post starts******** -->
-			<div class="post_style_A">
-
-				<div class="post_text_inside">
-
-					<a href="">
-						<h5 class="post-heading">CSULA Career Fair Event!</h5>
-					</a>
-
-					<p class="post_description">Cal State LA is hosting its annual career fair tomorrow. Come by and explore career opportunities with various companies
-						and see if you find something you might be interested in.</p>
-
-					<div class="date">
-						<a href="" class="post_sub_details_links">
-							<i class="fa fa-calendar"></i> August 15, 2018</a>
-						<!-- <a href="">
-										<i class="fa fa-comments-o"></i> 05</a> -->
-					</div>
-
-				</div>
-
-			</div>
-			<!-- ******** this is where individual post ends******** -->
-
-			<div class="post_style_A">
-
-				<div class="post_text_inside">
-
-					<a href="">
-						<h5 class="post-heading">ECST Barbeque (Freshmen and Jouniors)</h5>
-					</a>
-
-					<p class="post_description">The department of ECST is hosting its annual Barbeque. Come by and explore career opportunities with various companies
-						and see if you find something you might be interested in.</p>
-
-					<div class="date">
-						<a href="" class="post_sub_details_links">
-							<i class="fa fa-calendar"></i> August 25, 2018</a>
-						<!-- <a href="">
-											<i class="fa fa-comments-o"></i> 05</a> -->
-					</div>
-
-				</div>
-
-			</div>
+			<IndividualSsu></IndividualSsu>
+			<IndividualSsu></IndividualSsu>
+			<IndividualSsu></IndividualSsu>
+			<IndividualSsu></IndividualSsu>
 
 		</div>
 
@@ -56,9 +16,13 @@
 
 
 <script>
+	import IndividualSsu from './IndividualSsu'
 
 	export default {
-	name: 'SsuList'
+		name: 'SsuList',
+		components: {
+			IndividualSsu
+		}
 	}
 </script>
 
