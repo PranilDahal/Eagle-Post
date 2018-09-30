67bcad2552c136beed508349944ff71f1c32a43a
PranilDahal <dahalpranil@gmail.com>
2018-09-30 14:08:19 -0700

    Created all the skeleton methods for Student's coding tasks

--- backend/src/main/java/acm/objects/database/controllers/SsuControllers.java
+++ backend/src/main/java/acm/objects/database/controllers/SsuControllers.java
@@ -43,7 +43,7 @@ public class SsuControllers {
 	public SimpleStatusUpdate getSsuFromId(@PathVariable("ssuId") String ssuId) {
 		return SsuDatabaseBean.getById(ssuId);
 	}
-	
+
 	/**
 	 * @param DataToAdd - A SsuPostData object
 	 * @return POST - http://localhost:8080/SSU/SSU | Returns id of the new SSU if successfully inserted | Returns -1 if failed to insert SSU.
@@ -54,6 +54,52 @@ public class SsuControllers {
 		String id = SsuDatabaseBean.insertToDatabase(DataToAdd);
 
 		return Collections.singletonMap("id", id+"");
+	}
+
+
+	/**
+	 * @param userid - id to identify a user
+	 * @return GET - http://localhost:8080/SSU/byuser/{userid} | Returns all the SSUs posted by a specific userid
+	 */
+	@RequestMapping(value="/byuser/{userid}", method = RequestMethod.GET)
+	public List<SimpleStatusUpdate> getSsuPostedByAUser(@PathVariable("userid") String userid) {
+		// TODO Issue #31
+
+		// The parameter that this method receives is the userid that we wan't to query for in the database.
+		// Look at the other @RequestMapping methods in this file, and understand the way SsuFactory.java is being used.
+		// This method should call the getAllByUserId(String userid) method you created in the SsuFactory.java to get the values.
+
+		// *** TEST IF THE API WORKS *** Build and run the server and call the URL to test if it returns data.
+		// *** Stay in touch with whoever is doing Issue #30. SSUs don't have a userid identifiers yet. Issue #30 implements userid 
+		// for SSU class. Have him/her merge his code first, then test your code. OR work together in the same branch with him/her.***
+		return null;
+	}
+
+	
+	/**
+	 * @param ssuid - the unique ssuid of the SSU to delete
+	 * @return DELETE - http://localhost:8080/SSU/{userid} - Returns the id of the SSU deleted, or -1 if unsuccessful
+	 */
+	@RequestMapping(value="/{ssuid}", method = RequestMethod.DELETE)
+	public int deleteById(@PathVariable("ssuid") String ssuid) {
+		// TODO Issue #16
+
+		// this method should call deleteById(String ssuId) inside SsuFactory and return the result.
+		return -1;
+	}
+	
+	
+	/**
+	 * @param ssuid - the unique ssuid of the SSU to delete | dataToUpdate - The updated SSU datahandler object
+	 * @return PUT - http://localhost:8080/SSU/{userid} - Returns the UPDATED ssu
+	 */
+	@RequestMapping(value="/{ssuid}", method = RequestMethod.PUT)
+	public SimpleStatusUpdate updateById(@PathVariable("ssuid") String ssuid, @RequestBody SsuPostData dataToUpdate) {
+		// TODO Issue #17
+		
+		// When frontend calls this URL, it sends a SSU object which gets mapped to the SsuPostData object
+		// this method should call updateById(String ssuId, SsuPostData dataToUpdate) inside SsuFactory.java
+		return null;
 	}
 
 }
