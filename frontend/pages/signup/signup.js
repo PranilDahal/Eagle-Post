e97b1fa37867f766f2af35c6cdd2bdc3f0c923fe
David Tang <tangd954@gmail.com>
2018-10-09 16:18:20 -0700

    Added function to check username length

--- frontend/pages/signup/signup.js
+++ frontend/pages/signup/signup.js
@@ -1,1 +1,39 @@
-// Singup page javascript goes here
+//Singup page javascript goes here
+function checkUserName(username) {
+	if (username.length() == 10) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function checkPassword() {
+	
+}
+
+function checkCIN(CIN) {
+	if (CIN.length == 10) {
+		return CIN;
+	}
+	else if (CIN.length == 0) {
+		CIN = 0000000000
+		return CIN;
+	}
+}
+
+function checkFirstName() {
+	
+}
+
+function checkLastName() {
+	
+}
+
+function checkPhoneNumber() {
+	
+}
+
+function checkBirthday() {
+
+}
