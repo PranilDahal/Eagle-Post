716bad7070feb18cb4d3ce580e97422d5a458ba8
Lynne <R0ckisland@msn.com>
2018-10-23 21:23:05 -0700

    commited some task: Have a button called Login that user can press after entering their information (they should be able to press enter to trigger it)

--- frontend/pages/login/main.js
+++ frontend/pages/login/main.js
@@ -5,6 +5,38 @@
 // Leave the function empty since we dont have the backend ready yet to receive the login requests.
 // it should look like :
 
+//element.addEventListener("click", clickLogin(){ alert("Loging in..."); });
+
+// http://www.learningaboutelectronics.com/Articles/How-to-retrieve-data-from-a-text-box-in-javascript.php 
+//var username = document.getElementById("username").value
+
+	
+function enterUnicodeKey(event) {
+	var enterKey = event.keycode;
+	if (event.which || event.keyCode);	{//using the enter unicode key, which is for ff users
+		alert("login button pressed");
+		clickLogin();
+	}
+}
+
+
+	
+function clickLogin() {
+	alert ("Loging in...");	 // just checking if this function works
+/*
+	var userN = document.getElementById("username-input").value;
+	var passW = document.getElementById("password").value;
+    
+	if (userN == ""){
+		alert("no username entered");
+		return;
+	}
+	else{
+		return;
+	}*/
+}
+
 function login(username, password) {
 
 }
+
