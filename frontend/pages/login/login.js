e6b920a61fcda86e9076edc1d2b47da9fed35ee6
Kevin J Crespin <kevinjonathan290@gmail.com>
2018-10-23 22:17:24 -0700

    Finished, renamed html and js file to assigned names, all code is now functional, optimized, and clean

---
+++ frontend/pages/login/login.js
@@ -0,0 +1,13 @@
+
+function login(username, password) {
+
+    if (username == "" || password == ""){
+        alert("Not field should be left in blank");
+        return false;
+    }
+    else if (username.match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/)) {
+        alert("Cannot login using email");
+        return false;
+    }
+
+}
